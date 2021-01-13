cc6b1af3219337496251ebbbe040b699de72f58d
Takeru Ohta <phjgt308@gmail.com>
2021-01-13 19:18:26 +0900

    Add example

---
+++ examples/get-executions.rs
@@ -0,0 +1,20 @@
+use mlmd::MetadataStore;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Opt {
+    database_uri: String,
+}
+
+#[async_std::main]
+async fn main() -> anyhow::Result<()> {
+    let opt = Opt::from_args();
+    let mut store = MetadataStore::new(&opt.database_uri).await?;
+    println!("Connected");
+    println!();
+
+    for execution in store.get_executions().execute().await? {
+        println!("{:?}", execution);
+    }
+    Ok(())
+}
